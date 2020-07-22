c45f1ff95016a91d16f0eb098a2d9add9a728d51
qiangyin <qiangyin@sohu-inc.com>
2020-07-22 23:14:07 +0800

    Test

---
+++ DuilibExec/Test/main.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+
+class CDuiFrameWnd : public CWindowWnd, public INotifyUI
+{
+public:
+	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
+	virtual void Notify(TNotifyUI& msg) {}
+
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		LRESULT lRes = 0;
+
+		if (uMsg == WM_CREATE)
+		{
+			CControlUI *pWnd = new CButtonUI;
+			pWnd->SetText(_T("Hello World"));
+			pWnd->SetBkColor(0xff00ff00);
+
+			m_PaintManager.Init(m_hWnd);
+			m_PaintManager.AttachDialog(pWnd);
+			return lRes;
+		}
+
+		if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
+		{
+			return lRes;
+		}
+
+		return __super::HandleMessage(uMsg, wParam, lParam);
+	}
+
+protected:
+	CPaintManagerUI m_PaintManager;
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.ShowModal();
+	return 0;
+}
