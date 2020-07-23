c537ad3327b476a0e011861d48cc7c7155990cbb
qiangyin <qiangyin@sohu-inc.com>
2020-07-23 18:07:36 +0800

    自绘标题栏

--- DuilibExec/XmlTest/main.cpp
+++ DuilibExec/XmlTest/main.cpp
@@ -1,55 +1,14 @@
 #include "stdafx.h"
 
-class CDuiFrameWnd : public CWindowWnd, public INotifyUI
-{
+class CDuiFrameWnd : public WindowImplBase {
 public:
 	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
-	virtual void Notify(TNotifyUI& msg) {
-		if (msg.sType == _T("click") && msg.pSender->GetName() == _T("btnHello")) {
-			::MessageBox(NULL, _T("我是按钮"), _T("点击了按钮"), NULL);
-		}
-	}
-
-	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) {
-		LRESULT lRes = 0;
-
-		if (uMsg == WM_CREATE) {
-			m_PaintManager.Init(m_hWnd);
-
-			CDialogBuilder builder;
-			CControlUI* pRoot = builder.Create(_T("xmltest.xml"), 0, NULL, &m_PaintManager);
-
-			m_PaintManager.AttachDialog(pRoot);
-			m_PaintManager.AddNotifier(this);
-			return lRes;
-		}
-		else if (uMsg == WM_NCACTIVATE) {
-			if (!::IsIconic(m_hWnd))
-			{
-				return (wParam == 0) ? TRUE : FALSE;
-			}
-		}
-		else if (uMsg == WM_NCCALCSIZE) {
-			return 0;
-		}
-		else if (uMsg == WM_NCPAINT) {
-			return 0;
-		}
-
-		if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes)) {
-			return lRes;
-		}
-
-		return __super::HandleMessage(uMsg, wParam, lParam);
-	}
-
-protected:
-	CPaintManagerUI m_PaintManager;
+	virtual CDuiString GetSkinFile() { return _T("xmltest.xml"); }
+	virtual CDuiString GetSkinFolder() { return _T(""); }
 };
 
 int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) {
 	CPaintManagerUI::SetInstance(hInstance);
-	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath());
 
 	CDuiFrameWnd duiFrame;
 	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
